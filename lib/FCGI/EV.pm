4a8a2bd81c01c1228d4e2be90bb76c1cbcf853b7
Alex Efros <powerman@powerman.name>
2016-02-19 20:21:54 +0200

    migrate to Dist::Milla

--- lib/FCGI/EV.pm
+++ lib/FCGI/EV.pm
@@ -1,12 +1,12 @@
 package FCGI::EV;
-
+use 5.010001;
 use warnings;
 use strict;
+use utf8;
 use Carp;
 
-use version; our $VERSION = qv('1.0.9');    # update POD & Changes & README
+our $VERSION = 'v1.0.9';
 
-# update DEPENDENCIES in POD & Makefile.PL & README
 use Scalar::Util qw( weaken );
 use IO::Stream;
 
@@ -211,6 +211,8 @@ sub _pack_pkt {
 1; # Magic true value required at end of module
 __END__
 
+=encoding utf8
+
 =head1 NAME
 
 FCGI::EV - Implement FastCGI protocol for use in EV-based applications
@@ -218,7 +220,7 @@ FCGI::EV - Implement FastCGI protocol for use in EV-based applications
 
 =head1 VERSION
 
-This document describes FCGI::EV version 1.0.9
+This document describes FCGI::EV version v1.0.9
 
 
 =head1 SYNOPSIS
@@ -431,63 +433,63 @@ While parsing data from web server error %s happened and connection was closed.
 =back
 
 
-=head1 CONFIGURATION AND ENVIRONMENT
+=head1 SUPPORT
 
-FCGI::EV requires no configuration files or environment variables.
+=head2 Bugs / Feature Requests
 
+Please report any bugs or feature requests through the issue tracker
+at L<https://github.com/powerman/perl-FCGI-EV/issues>.
+You will be notified automatically of any progress on your issue.
 
-=head1 DEPENDENCIES
+=head2 Source Code
 
- IO::Stream
- EV
+This is open source software. The code repository is available for
+public review and contribution under the terms of the license.
+Feel free to fork the repository and submit pull requests.
 
+L<https://github.com/powerman/perl-FCGI-EV>
 
-=head1 INCOMPATIBILITIES
+    git clone https://github.com/powerman/perl-FCGI-EV.git
 
-None reported.
+=head2 Resources
 
+=over
 
-=head1 BUGS AND LIMITATIONS
+=item * MetaCPAN Search
 
-No bugs have been reported.
+L<https://metacpan.org/search?q=FCGI-EV>
 
-Please report any bugs or feature requests to
-C<bug-fcgi-ev@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/dist/FCGI-EV>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/FCGI-EV>
+
+=item * CPAN Testers Matrix
+
+L<http://matrix.cpantesters.org/?dist=FCGI-EV>
+
+=item * CPANTS: A CPAN Testing Service (Kwalitee)
+
+L<http://cpants.cpanauthors.org/dist/FCGI-EV>
+
+=back
 
 
 =head1 AUTHOR
 
-Alex Efros  C<< <powerman-asdf@ya.ru> >>
+Alex Efros E<lt>powerman@cpan.orgE<gt>
 
 
-=head1 LICENSE AND COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2009, Alex Efros C<< <powerman-asdf@ya.ru> >>. All rights reserved.
+This software is Copyright (c) 2009 by Alex Efros E<lt>powerman@cpan.orgE<gt>.
 
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
+This is free software, licensed under:
+
+  The MIT (X11) License
 
 
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+=cut
